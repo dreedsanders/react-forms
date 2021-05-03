15ec9b9502c62b64e157e2fec256215fba3e83ca
github username <github email address>
2021-05-03 13:31:10 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import DisplayData from './components/DisplayData';
 import Form from './components/Form'
+import ParentComponent from './components/ParentComponent';
 
 ReactDOM.render(
   <div>
-    <Form />
+    <ParentComponent />
   </div>,
   document.getElementById('root')
 );
